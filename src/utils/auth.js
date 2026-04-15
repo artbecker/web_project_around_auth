c69d194109c223c0efc133f38c030e1448fef276
ArtBecker <arthurk.becker@gmail.com>
2026-04-15 09:43:37 -0300

    Initial structure

---
+++ src/utils/auth.js
@@ -0,0 +1,1 @@
+export const BASE_URL = 'https://se-register-api.en.tripleten-services.com/v1';
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/web_project_around_react/",
-  build: { outDir: "docs" },
+  build: { outDir: 'docs' },
   server: {
     port: 3000,
   },
